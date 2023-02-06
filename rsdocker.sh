57f09854e74c265bc8585e3ed062dcd43b6e39cb
Tiago Barretto <tiagobarreto581@gmail.com>
2023-02-06 11:09:30 -0300

    Fix docker errors

--- rsdocker.sh
+++ rsdocker.sh
@@ -1,7 +1,7 @@
 export ROS_DOMAIN_ID=200
-source /opt/ros/foxy/setup.bash
+source /opt/ros/humble/setup.bash
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-export FASTRTPS_DEFAULT_PROFILES_FILE=$PWD/super_client_configuration_file_docker.xml
+export FASTRTPS_DEFAULT_PROFILES_FILE=super_client_configuration_file_docker.xml
 export ROS_DISCOVERY_SERVER=192.168.1.102:11811
 export RMW_FASTRTPS_USE_QOS_FROM_XML=1
 ros2 daemon stop
