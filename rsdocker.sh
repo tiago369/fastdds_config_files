759a48349fcbc89405d8e8ad3acec5d58ee51e2e
tiago369 <tiagobarreto581@gmail.com>
2023-02-27 09:57:59 -0300

    Fix source

--- rsdocker.sh
+++ rsdocker.sh
@@ -2,6 +2,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 export ROS_DOMAIN_ID=200
 source /opt/ros/foxy/setup.bash
+source ../$parent_path/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export FASTRTPS_DEFAULT_PROFILES_FILE=$parent_path/super_client_configuration_file_docker.xml
 export ROS_DISCOVERY_SERVER=192.168.1.102:11811
--- rsmachine.sh
+++ rsmachine.sh
@@ -2,7 +2,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 export ROS_DOMAIN_ID=200
 source /opt/ros/foxy/setup.bash
-source $HOME/subot_ssd/gps_ws/install/setup.bash
+source ../$parent_path/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export FASTRTPS_DEFAULT_PROFILES_FILE=$parent_path/super_client_configuration_file_machine.xml
 export ROS_DISCOVERY_SERVER=192.168.1.102:11811
