6fe0dbe327f5d706dddbf892e61389ba3096ccfa
tiago369 <tiagobarreto581@gmail.com>
2023-02-27 13:41:20 -0300

    Test

--- rsdocker.sh
+++ rsdocker.sh
@@ -1,6 +1,6 @@
 source setup.sh
 export FASTRTPS_DEFAULT_PROFILES_FILE=$parent_path/super_client_configuration_file_docker.xml
 export ROS_DISCOVERY_SERVER=192.168.1.102:11811
-export RMW_FASTRTPS_USE_QOS_FROM_XML=0
+export RMW_FASTRTPS_USE_QOS_FROM_XML=1
 ros2 daemon stop
 ros2 daemon start
