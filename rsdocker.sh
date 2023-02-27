35b439ec72bbc39cf9485bcadad8b75e425fcec2
tiago369 <tiagobarreto581@gmail.com>
2023-02-27 09:55:52 -0300

    Fix files

--- rsdocker.sh
+++ rsdocker.sh
@@ -4,7 +4,7 @@ export ROS_DOMAIN_ID=200
 source /opt/ros/foxy/setup.bash
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export FASTRTPS_DEFAULT_PROFILES_FILE=$parent_path/super_client_configuration_file_docker.xml
-export ROS_DISCOVERY_SERVER=10.88.119.12:11811
+export ROS_DISCOVERY_SERVER=192.168.1.102:11811
 export RMW_FASTRTPS_USE_QOS_FROM_XML=0
 ros2 daemon stop
 ros2 daemon start
