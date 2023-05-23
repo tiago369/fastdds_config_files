4d5b74370cdea7a6552fefb6a17ff812eb02809f
tiago369 <tiagobarreto581@gmail.com>
2023-05-23 10:28:19 -0300

    Improve settings

--- setup.sh
+++ setup.sh
@@ -7,6 +7,9 @@ sudo sysctl net.ipv4.ipfrag_time=3
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 export ROS_DOMAIN_ID=200
+export SERVER_IP=192.168.1.102
+export SERVER_PORT=11811
+alias run_server="fastdds discovery -i 0 -p $SERVER_PORT -l $SERVER_IP"
 source /opt/ros/$ROS_DISTRO/setup.bash
 source ../$parent_path/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
